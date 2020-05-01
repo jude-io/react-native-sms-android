3385e71b972352825b05b6f0977e5a88da0b3e9e
Oliver Fawcett-Griffiths <olly@ollyfg.com>
2020-05-01 15:40:06 +1200

    Make it work for us

--- android/src/main/java/com/rhaker/reactnativesmsandroid/RNSmsAndroidPackage.java
+++ android/src/main/java/com/rhaker/reactnativesmsandroid/RNSmsAndroidPackage.java
@@ -1,32 +1,23 @@
 package com.rhaker.reactnativesmsandroid;
 
-import android.app.Activity;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNSmsAndroidPackage implements ReactPackage {
-    private RNSmsAndroidModule mModuleInstance;
-
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      mModuleInstance = new RNSmsAndroidModule(reactContext);
-      return Arrays.<NativeModule>asList(mModuleInstance);
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
+        return Arrays.<NativeModule>asList(new RNSmsAndroidModule(reactContext));
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
-
 }
